035aa57dca47af10ab9aed96809ce36e045a320e
rhossi <felipecpg@gmail.com>
2024-06-23 08:24:09 -0400

    * using GPT-40 * using postgresql with full dump * refined prompt to format final values to Brazilian locale

--- api.py
+++ api.py
@@ -16,46 +16,49 @@ from langchain_core.prompts import (
     SystemMessagePromptTemplate,
 )
 
-db = SQLDatabase.from_uri("sqlite:///syny.db", sample_rows_in_table_info=3)
+db_uri = "postgresql://postgres:d9q4Juye$e@synydb.crae42w04nzr.us-east-1.rds.amazonaws.com:5432/postgres"
+db = SQLDatabase.from_uri(db_uri, sample_rows_in_table_info=3)
 
 examples = [
     {
         "input": "Quais dispositivos fazemos coleta?", 
-        "query": "SELECT distinct type from historic_data"
+        "query": "SELECT distinct type from device_twin_variable_histories"
     },
     {
         "input": "Qual o consumo total de gas?",
-        "query": "SELECT SUM(value) FROM historic_data WHERE type = 'gas'",
+        "query": "SELECT SUM(CAST(value AS numeric)) FROM device_twin_variable_histories WHERE type = 'gas'"
     },
     {
         "input": "Qual o consumo total de agua em maio de 2023?",
-        "query": """
-            SELECT SUM(value)
-            FROM historic_data
-            WHERE strftime('%Y-%m', created_at) = '2023-05'
-            AND type = 'water';
-        """,
+        "query": r"""
+            SELECT SUM(CAST(value AS numeric))
+            FROM device_twin_variable_histories
+            WHERE created_at between '2023-05-01' and '2023-05-31'
+            AND type = 'water'
+        """
     },
     {
         "input": "Agrupado por serviço, me diga qual mês e ano tive o consumo mais alto?",
-        "query": """
+        "query": r"""
             WITH MonthlyConsumption AS (
-                SELECT type, strftime('%Y-%m', created_at) AS month_year, SUM(value) AS total_consumption
-                FROM historic_data
+                SELECT type, to_char(created_at, 'YYYY-MM') AS month_year, SUM(CAST(value AS numeric)) AS total_consumption
+                FROM public.device_twin_variable_histories
+                WHERE value ~ '^[0-9]+(\.[0-9]+)?$'
                 GROUP BY type, month_year
             ),
             MaxConsumption AS (
-                SELECT month_year, MAX(total_consumption) AS max_consumption
+                SELECT type, month_year, MAX(total_consumption) AS max_consumption
                 FROM MonthlyConsumption
-                GROUP BY month_year
-                ORDER BY max_consumption DESC
-                LIMIT 1
+                GROUP BY type, month_year
             )
-            SELECT DISTINCT h.type, strftime('%Y-%m', h.created_at) AS month_year, SUM(h.value) AS total_consumption
-            FROM historic_data h
-            JOIN MaxConsumption m ON strftime('%Y-%m', h.created_at) = m.month_year
-            GROUP BY h.type, month_year;
-        """,
+            SELECT type, month_year, total_consumption
+            FROM (
+                SELECT type, month_year, total_consumption,
+                    ROW_NUMBER() OVER (PARTITION BY type ORDER BY total_consumption DESC) AS rn
+                FROM MonthlyConsumption
+            ) AS ranked
+            WHERE rn = 1;
+        """
     }
 ]
 
@@ -80,9 +83,14 @@ You MUST double check your query before executing it. If you get an error while 
 
 DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.) to the database.
 
-If the question does not seem related to the database or you don't know the answer, just return "I don't know" as the answer.
+If the user does not specify the want the consumption in units, always assume it is in Brazilian Reais (R$)
 
-Final answer should be fully translated to Portuguese (Brazil).
+Final answer should be fully translated to Portuguese (Brazil). Specify the format for numbers and currency:
+
+In Brazil, numbers use a comma , as the decimal separator and a period . as the thousand separator.
+Currency is typically represented with the symbol R$, placed before the number.
+
+If the question does not seem related to the database or you don't know the answer, just return "I don't know" as the answer.
 
 Here are some examples of user inputs and their corresponding SQL queries:"""
 
@@ -102,17 +110,7 @@ full_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-# Example formatted prompt
-prompt_val = full_prompt.invoke(
-    {
-        "input": "Qual o consumo de gas esse mes?",
-        "top_k": 5,
-        "dialect": "SQLite",
-        "agent_scratchpad": [],
-    }
-)
-
-llm = ChatOpenAI(temperature=0)
+llm = ChatOpenAI(temperature=0, model="gpt-4o")
 
 agent = create_sql_agent(
     llm=llm,
